ee21ae953ec96b42962719b4e84bb3c9f9ec86be
c0b22147f6 <c0b22147f6@edu.teu.ac.jp>
2023-12-05 14:39:15 +0900

    練習２　こうかとんが喜ぶエフェクトの追加

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -170,6 +170,7 @@ def main():
             if beam.rct.colliderect(bomb.rct):
                 beam = None
                 bomb = None
+                bird.change_img(6,screen)
 
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
